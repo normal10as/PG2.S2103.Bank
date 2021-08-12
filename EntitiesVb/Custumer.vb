9d6427b955170ab7103bb136fedf2b47134c4056
Fabio Dos Santos <fabio2st@yahoo.com>
2021-08-12 12:18:38 -0300

    Todas las propiedades

--- EntitiesVb/Custumer.vb
+++ EntitiesVb/Custumer.vb
@@ -1,19 +1,42 @@
 Public Class Custumer
     Public Sub New()
         Nombre = ""
+        Documento = 0
+        FechaNacimiento = Nothing
     End Sub
-    Private _nombre As String
-    Private _documento As Integer
-    Private _fechaNacimiento As DateTime
 
+    ' Campo, este forma parte de la implementación privada
+    Private _nombre As String
+    ' Propiedad, forma parte de la interfaz pública
     Public Property Nombre As String
+        ' Metodo Getter es para obtener el valor
         Get
             Return _nombre
         End Get
+        ' Método Setter es para asignar el valor
         Set(value As String)
             ' aqui se pueden hacer validaciones
             _nombre = value.ToUpper()
         End Set
     End Property
 
+    Private _documento As Integer
+    Public Property Documento As Integer
+        Get
+            Return _documento
+        End Get
+        Set(value As Integer)
+            _documento = value
+        End Set
+    End Property
+
+    Private _fechaNacimiento As Date
+    Public Property FechaNacimiento As Date
+        Get
+            Return _fechaNacimiento
+        End Get
+        Set(value As Date)
+            _fechaNacimiento = value
+        End Set
+    End Property
 End Class
